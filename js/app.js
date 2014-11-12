bfc8ec20018b4e935ad57a14db0c30d84256941a
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2014-11-12 12:03:08 +0300

    added mealy input table

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 angular.module('app', [
-    'ngRoute'
+    'ngRoute',
+    'ui.bootstrap'
 ])
     .config(function($routeProvider){
         $routeProvider
@@ -16,10 +17,25 @@ angular.module('app', [
     })
     .controller('NavCtrl', function NavCtrl(){
     })
-    .controller('MealyCtrl', function MealyCtrl(){
+    .controller('MealyCtrl', function MealyCtrl($scope){
+        var ctrl = this;
         this.numberOfA = 1;
         this.numberOfZ = 1;
         this.numberOfW = 1;
+        this.a = [];
+        this.z = [];
+        this.w = [];
+        this.$watchNumbers = function(numberCallback, collection, name){
+            $scope.$watch(numberCallback, function(val){
+                collection.splice(0, collection.length);
+                for(var i = 0; i<val; ++i){
+                    collection.push(name+(i+1));
+                }
+            });
+        };
+        this.$watchNumbers(function(){ return ctrl.numberOfA}, ctrl.a, 'a');
+        this.$watchNumbers(function(){ return ctrl.numberOfZ}, ctrl.z, 'z');
+        this.$watchNumbers(function(){ return ctrl.numberOfW}, ctrl.w, 'w');
     })
     .controller('MooreCtrl', function MooreCtrl(){
 
